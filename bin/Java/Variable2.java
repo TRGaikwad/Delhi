f945371458ea81ece4b303d04e3fa13ec64c6568
TRGaikwad <endlessitlearning@gmail.com>
2022-10-10 15:27:10 +0530

    Please check

---
+++ bin/Java/Variable2.java
@@ -0,0 +1,25 @@
+package Oct6;
+
+public class Variable2 {
+	int a;
+	int b;
+
+	public void show() {
+		System.out.println(a);
+		System.out.println(b);
+	}
+       public void Delete() {
+		System.out.println("Ptint");
+		System.out.println("Print");
+	}
+
+
+	public static void main(String[] args) {
+
+		Variable2 v2 = new Variable2();
+		v2.a = 10;
+		v2.b = 20;
+
+	}
+
+}
